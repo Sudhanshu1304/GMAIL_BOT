ca28c21b517a422f17fb2154105ae1c3a9f5a032
sudhanshu <sudhanshu.dpandey@gmail.com>
2020-11-07 21:53:39 +0530

    Updating the Email algo and readme

--- Gmail_Bot.py
+++ Gmail_Bot.py
@@ -1,110 +1,130 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import smtplib
-import  random
+import random
 
+path= "Enter the PATH TO STORE THE EMAIL IN "
 
 
 def From(email):
 
     try:
-        file=open('D:/User/Desktop/MY APPS/Gmail_Bot/Email.txt','r')
+        file = open(path, 'r')
         content = file.read()
 
         content = content.split('\n')
 
         if email not in content:
             file.close()
-            file = open('D:/User/Desktop/MY APPS/Gmail_Bot/Email.txt', 'a')
-            file.write(email+"\n")
+            file = open(path, 'a')
+            file.write(email + "\n")
         file.close()
 
     except:
-        file = open('D:/User/Desktop/MY APPS/Gmail_Bot/Email.txt', 'a')
+        file = open(path, 'a')
         file.write(email + "\n")
         file.close()
 
 
-
-
 def Options():
 
-    global content,opt
+    global content, opt
     global gmail_user, gmail_password
 
-    try :
+    try:
 
-        file=open("D:/User/Desktop/MY APPS/Gmail_Bot/Email.txt",'r')
-        content=file.read()
-        content=content.split('\n')
+        file = open(path, 'r')
+        content = file.read()
+        content = content.split('\n')
 
-        c=0
+        c = 0
         for i in range(len(content)):
-            if len(content[i])!=0 :
-                print('\n',' ',c,' : ',content[i])
-                c=c+1
+            if len(content[i]) != 0:
+                print('\n', ' ', c, ' : ', content[i])
+                c = c + 1
 
         file.close()
 
-        print('\n',c," : OTHER ")
-        opt=int(input("\nChoice : "))
-        
-        if opt !=(c):
+        print('\n', c, " : OTHER ")
+        opt = int(input("\nChoice : "))
+
+        if opt != (c):
             gmail_user = content[opt]
-           
+
         else:
             gmail_user = input("\nEnter Your Email : ")
             From(gmail_user)
 
     except:
 
-        gmail_user=input("\nEnter Your Email : ")
+        gmail_user = input("\nEnter Your Email : ")
         From(gmail_user)
+
 
 Options()
 
-global content,opt
+msgg = MIMEMultipart()
 
-l=0
-inp=0
+msgg['From'] =gmail_user
 
-iter=int(input("\nNo of Times : "))
+
+global content, opt
+
+l = 0
+inp = 0
+
+iter = int(input("\nNo of Times : "))
 gmail_password = input('\nEnter Password : ')
 sent_from = gmail_user
 
-TO=input("\nEmail TO : ")
-From(TO)
+TOO=[]
+TO='initializing  TO'
+while(TO!='-1'):
+    TO = input("\nEmail TO (enter \'-1\' to exit ): ")
+    TOO.append(TO)
+    From(TO)
 
-msg=input("\nEnter The message  : ")
+print('\nEnter Message  Body :  ')
+
+lines = []
+while True:
+    line = input()
+    if line:
+        lines.append("\n")
+        lines.append(line)
+    else:
+        break
+
+text = ''.join(lines)
 
 print("\n1. Subject Random Number\n2. EDIT \n")
-o=int(input('Enter Your Choice : '))
+o = int(input('Enter Your Choice : '))
 
-if o==2:
-    subject=input("\nEnter SUBJECT : ")
+if o == 2:
+    subject = input("\nEnter SUBJECT : ")
 else:
     subject = '#$$*{}$$%'.format(random
-                          .randint(100000000,99999999999))
-        
+                                 .randint(100000000, 99999999999))
 
-for _ in range(iter):
+msgg['Subject']=subject
 
-    to = [ TO ]
-    body=msg
+for i in range(iter):
 
-    email_text = """From: %s
-    To: %s\n
-    Subject: %s\n
+    for to in TOO:
+        msgg['To'] = to
+        body = text
+        body = MIMEText(body)
+        msgg.attach(body)
 
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
+        text = msgg.as_string()
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com')
+            server.ehlo()
+            server.login(gmail_user, gmail_password)
+            server.sendmail(sent_from, to,text)
+            server.close()
 
-    try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com')
-        server.ehlo()
-        server.login(gmail_user, gmail_password)
-        server.sendmail(sent_from, to, email_text)
-        server.close()
-
-        print ('\nEmail sent!')
-    except:
-        print ('Something went wrong...')
+            print('\nEmail sent!')
+        except:
+            print('Something went wrong...')
 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# GMAIL_BOT
-This Program Automates GMAIL
+# Automate Your Gmail 
+
+> Description <br>
+Using this Program you can send Emails to Multiple users at the same time, also it is possible to send the same mail
+multiple times .
+
+<br><br>
+
+### **Features**
+
+<br>
+
+* This program creates a simple database within  users device which stores the Email address of the users ones entered which helps in avoiding writing email adresses every time.<br><br>
+![](select.png)
+
+<br>
+
+* 
